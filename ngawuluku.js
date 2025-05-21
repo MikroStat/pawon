153489436589e5c3ce1062bd3107ce4a20671268
MikroStat <alna.mikrostat@gmail.com>
2025-05-21 22:26:32 +0700

    Update ngawuluku.js

--- ngawuluku.js
+++ ngawuluku.js
@@ -11,8 +11,8 @@ function spRepeat(jml){
 function idSapasi(inpId) {
 	const pj = inpId.length;
 	const idMax = 14,
-			codeNol = 48,
-			codeSembilan = 57;
+		codeNol = 48,
+		codeSembilan = 57;
 			
 	let sp = '';
 	if (pj <= idMax) sp = inpId + '\"' + spRepeat(idMax-pj);
@@ -27,4 +27,31 @@ function idSapasi(inpId) {
 	}	
 	sp += '>';
 	return sp;
+}
+
+// menyusun kembali kode menjadi directLink
+// input : idName atau idNomerUrut
+// output [0] : id
+//		  [1] : directLink
+//        [2] : deskripsi
+function linkMulih(inpKode) {
+	// deteksi tipe get.Element
+	let q = [];
+	let x, z;
+	
+	const y = inpKode.charCodeAt(0);
+	if (y >= 48 & y <= 57) {
+		inpKode -= 1; // meyesuaikan start [0]
+	   z = document.getElementsByTagName("rp")[inpKode].innerHTML;
+	   x = document.getElementsByTagName("rp")[inpKode].id;		
+	} else {
+		z  = document.getElementById(inpKode).innerHTML;
+		x  = document.getElementById(inpKode).id;	
+	}
+	let c = z.split('/');
+	q[0] = x;
+	q[1] = c[0];
+	q[2] = c[1];
+	
+	return q;
 }
