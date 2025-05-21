b48fd4a13fe5ef5645845192dd99a966c8d5a30b
MikroStat <alna.mikrostat@gmail.com>
2025-05-21 21:50:16 +0700

    Update garu.js

--- garu.js
+++ garu.js
@@ -6,3 +6,26 @@ function spRepeat(jml){
 	}
 	return sp;	
 }
+
+// nomenklatur iD supaya rapi
+function idSapasi(inpId) {
+	const pj = inpId.length;
+	const idMax = 14,
+			codeNol = 48,
+			codeSembilan = 57;
+			
+	let sp = '';
+
+	if (pj <= idMax) sp = inpId + '\"' + spRepeat(idMax-pj);
+		
+	// jika sebuah sitasi
+	if (pj > idMax) {
+		const chEnd = inpId.charCodeAt(pj-1);
+		if (chEnd >= codeNol & chEnd <= codeSembilan) {
+			const namaPenggal = inpId.substr(0, idMax-4);
+			sp = namaPenggal + inpId.substr(pj-4, 4) + '\"';	
+		}
+	}	
+	sp += '>';
+	return sp;
+}
