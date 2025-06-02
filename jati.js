66a1fa303a4b009346f893f599c6163c864c5bfe
MikroStat <alna.mikrostat@gmail.com>
2025-06-02 21:09:07 +0700

    Update jati.js

--- jati.js
+++ jati.js
@@ -31,97 +31,103 @@ async function copyToCb(idEnt) {
   await navigator.clipboard.writeText(teks);
 }
 
+function noInp() {
+  let e = '';
+  if (arguments[0] == undefined || arguments[0] == '') {
+	return e = 'Q';
+   } else e = arguments[0];
+	
+   return e;
+}
+
 // menambahkan spasi pada element
 function spDeui(jml){
-	let sp = '';
-	for (let i = 0; i < jml; i++) {
-		sp += '&nbsp;';
-	}
-	return sp;	
+  let sp = '';
+  for (let i = 0; i < jml; i++) {
+	sp += '&nbsp;';
+  }
+  return sp;	
 }
 
 // deteksi ch sebagai bilangan Arabic
 function chBil(ch){
-	const z = 48,  // kode 0
-		   s = 57,  // kode 9
-			c = ch.charCodeAt(0);
-			
-	let y = false;
-		 
-	if (c >= z & c <= s) y = true;	
-	return y;
+   const z = 48,  // kode 0
+	 s = 57,  // kode 9
+	 c = ch.charCodeAt(0);	
+   let   y = false;
+	
+   if (c >= z & c <= s) y = true;	
+   return y;
 }
 
 // deteksi ch sebagai bilangan Romawi
 function chRomawi(text){
-	const r = 'ivxlcdmIVXLCDM',
+   const r = 'ivxlcdmIVXLCDM',
          p = text.length;
-	let y = false;
+   let   y = false;
 
-	for (let i=0; i<p; i++) {
-		if( r.includes(text[i]) ) {
-			y = true;
-			break;
-		}	
-	}
-
-	return y;
+   for (let i=0; i<p; i++) {
+	if( r.includes(text[i]) ) {
+	   y = true;
+	   break;
+	}	
+   }
+   return y;
 }
 
+// menambahka spasi untuk kerapian list Id
 function idSapasi(e) {
-	const p = e.length,
-	      m = 12,  // jml maksimal kode buku
-			h = 9,   // jml space untuk nomor halaman
-			t = 4,   // jml huruf sebuah tahun
-			n = 4;   // jml maks penomoran halaman
+   const p = e.length,
+         m = 12,  // jml maksimal kode buku
+  	 h = 9,   // jml space untuk nomor halaman
+	 t = 4;   // jml huruf sebuah tahun
 			 
-	let sp = '',
-		 y  = '';  // dummy
-	// jika sebuah sitasi
-	if (p > m) {
-		if ( chBil(e.at(p-1))) {
-			// hanya mengambil sebagian nama
-			y = e.substr(0, m-t); 
-			sp = y + e.substr(p-t, t) + '\"';	
-		}
-	}	else {
-		y = m;
-		// halaman romawi tetap ditulis angka   
-		if ( chBil(e.at(0)) || chRomawi(e.at(0)) ) y = h;
-		if ( p == 1) {
-			e += 'Q';
-			y -= 1;
-		}
-		sp = e + '\"' + spDeui(y-p);
+   let  sp = '',
+	y  = '';  // dummy
+   // jika sebuah sitasi
+   if (p > m) {
+	if ( chBil(e.at(p-1))) {
+	   // hanya mengambil sebagian nama
+	   y = e.substr(0, m-t); 
+	   sp = y + e.substr(p-t, t) + '\"';	
 	}
-	sp += '>';
-	
-	return sp;
+    } else {
+	y = m;
+	// halaman romawi tetap ditulis angka   
+	if ( chBil(e.at(0)) || chRomawi(e.at(0)) ) y = h;
+	if ( p == 1) {
+	   e += 'Q';
+	   y -= 1;
+	}
+	sp = e + '\"' + spDeui(y-p);
+    }
+    sp += '>';
+    return sp;
 }
 
 // membaca element directLink
 // o [0] : id
-//	  [1] : directLink
+//   [1] : directLink
 //   [2] : deskripsi
 function metaLink(e) {
-	// deteksi tipe get.Element
-	const y = [];
-	let x, z;
+   // deteksi tipe get.Element
+   const y = [];
+   let x, z;
 
-	if ((e.length < 2) && chBil(e.at(0))) {
-	   // e -= 1; // meyesuaikan start [0]
-	   z = document.getElementsByTagName("rp")[e].innerHTML;
-	   x = document.getElementsByTagName("rp")[e].id;
-	} else {
-	   z  = document.getElementById(e).innerHTML;
-	   x  = document.getElementById(e).id;
-	}
-	const c = z.split('/');
-	y[0] = x;
-	y[1] = c[0];
-	y[2] = c[1];
+   if ((e.length < 2) && chBil(e.at(0))) {
+	// e -= 1; // meyesuaikan start [0]
+	z = document.getElementsByTagName("rp")[e].innerHTML;
+	x = document.getElementsByTagName("rp")[e].id;
+   } else {
+	z  = document.getElementById(e).innerHTML;
+	x  = document.getElementById(e).id;
+   }
+   const c = z.split('/');
+   y[0] = x;
+   y[1] = c[0];
+   y[2] = c[1];
 	
-	return y;
+  return y;
 }
 
 
